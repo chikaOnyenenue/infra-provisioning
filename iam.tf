17e383ecd8fc75e858340153ae9cc8d4ad62fa8c
Chika Onyenenue <chikaonyenenue@gmail.com>
2026-08-02 18:51:12 +0200

    creating password for  user chika-admin

--- iam.tf
+++ iam.tf
@@ -5,3 +5,9 @@ resource "aws_iam_user_policy_attachment" "admin_access" {
   user       = aws_iam_user.raw_data_playground_user.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+resource "aws_iam_user_login_profile" "chika_admin_login" {
+  user                    = aws_iam_user.raw_data_playground_user.name
+  password_reset_required = true
+
+  password = "chika1234567890"
+}
