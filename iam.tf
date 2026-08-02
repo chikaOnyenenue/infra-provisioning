339856d4b5ebedac75968ca64b3b755ad76d4790
Chika Onyenenue <chikaonyenenue@gmail.com>
2026-08-02 16:59:38 +0200

    creating user chika-admin

--- iam.tf
+++ iam.tf
@@ -2,7 +2,7 @@ resource "aws_iam_user" "raw_data_playground_user" {
   name = "chika-admin"
 }
 resource "aws_iam_user_policy" "raw_data_playground_user_policy" {
-  name = "chika-admin-policy"
-  user = aws_iam_user.raw_data_playground_user.name
-  policy="arn:aws:iam::aws:policy/AdministratorAccess"
+  name   = "chika-admin-policy"
+  user   = aws_iam_user.raw_data_playground_user.name
+  policy = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
