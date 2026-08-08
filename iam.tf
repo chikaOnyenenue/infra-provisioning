a2b981823b30968e4bb34e507d4b46303fc8d66e
Chika Onyenenue <chikaonyenenue@gmail.com>
2026-08-08 21:37:39 +0200

    creating a bucket and iam policy for airflow to read ssm and access to the created bucket

--- iam.tf
+++ iam.tf
@@ -5,7 +5,3 @@ resource "aws_iam_user_policy_attachment" "admin_access" {
   user       = aws_iam_user.raw_data_playground_user.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
-resource "aws_iam_user_login_profile" "chika_admin_login" {
-  user                    = aws_iam_user.raw_data_playground_user.name
-  password_reset_required = true
-}
